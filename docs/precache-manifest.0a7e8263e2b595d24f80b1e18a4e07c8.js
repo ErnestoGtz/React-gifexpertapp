ee31e09fbf7dae9cb757e1b68139488a6f875212
Ernesto Emmanuel Gutierrez Muñoz <ernesto.gtz@itcoach.com.mx>
2020-07-13 20:20:22 -0500

    Second Commit

---
+++ docs/precache-manifest.0a7e8263e2b595d24f80b1e18a4e07c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56141120391fc543a44ed0f7082e6065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e2e851689da3919a953",
+    "url": "/static/css/main.e9a0b20d.chunk.css"
+  },
+  {
+    "revision": "194a0661fc43625f51eb",
+    "url": "/static/js/2.0d42c3c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d42c3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2e851689da3919a953",
+    "url": "/static/js/main.6c96aafa.chunk.js"
+  },
+  {
+    "revision": "a77a76a56512bcd3f3df",
+    "url": "/static/js/runtime-main.f5bc3f16.js"
+  }
+]);
